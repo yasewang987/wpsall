2f5c6d04cbeb0de152ee065d56cdf646e9fd129a
zouyingfeng <zouyingfeng@wps.cn>
2020-01-08 19:59:03 +0800

    增加nodejs本地服务端程序，完善与服务端交互的演示场景

---
+++ oaassist/server/StartupServer.js
@@ -0,0 +1,125 @@
+﻿const express = require('express');
+const fs = require('fs');
+const path = require('path');
+var urlencode = require('urlencode');
+const formidable = require('formidable')
+
+const app = express()
+
+//设置允许跨域访问该服务.
+app.all('*', function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	//Access-Control-Allow-Headers ,可根据浏览器的F12查看,把对应的粘贴在这里就行
+	res.header('Access-Control-Allow-Headers', 'Content-Type');
+	res.header('Access-Control-Allow-Methods', '*');
+	res.header('Content-Type', 'application/json;charset=utf-8');
+	next();
+  });
+
+app.use("/FileList", function (request, response) {
+	var filePath = path.join(__dirname, './wwwroot/file');
+	console.log(filePath)
+	fs.readdir(filePath, function (err, results) {
+		if (err) {
+			response.writeHead(200, "OK", { "Content-Type": "text/html; charset=utf-8" });
+			response.end("没有找到file文件夹");
+			return;
+		}
+		if (results.length > 0) {
+			var files = [];
+			results.forEach(function (file) {
+				if (fs.statSync(path.join(filePath, file)).isFile()) {
+					files.push(file);
+				}
+			})
+			response.writeHead(200, "OK", { "Content-Type": "text/html; charset=utf-8" });
+			response.end(files.toString());
+		} else {
+			response.writeHead(200, "OK", { "Content-Type": "text/html; charset=utf-8" });
+			response.end("当前目录下没有文件");
+		}
+	});
+});
+app.use("/Download/:fileName", function (request, response) {
+	var fileName = request.params.fileName;
+	var filePath = path.join(__dirname, './wwwroot/file');
+	filePath = path.join(filePath, fileName);
+	console.log(filePath)
+	var stats = fs.statSync(filePath);
+	if (stats.isFile()) {
+		let name = urlencode(fileName, "utf-8");
+		response.set({
+			'Content-Type': 'application/octet-stream',
+			'Content-Disposition': "attachment; filename* = UTF-8''" + name,
+			'Content-Length': stats.size
+		});
+		fs.createReadStream(filePath).pipe(response);
+	} else {
+		response.writeHead(200, "Failed", { "Content-Type": "text/html; charset=utf-8" });
+		response.end("文件不存在");
+	}
+});
+app.post("/Upload", function (request, response) {
+	const form = new formidable.IncomingForm();
+	form.encoding = 'utf-8';
+	form.uploadDir = "tmp";
+	form.parse(request, function (error, fields, files) {
+		for (let key in files) {
+			let file = files[key]
+			// 过滤空文件
+			if (file.size == 0 && file.name == '') continue
+
+			let oldPath = file.path,
+				newPath = 'wwwroot/file/' + file.name
+
+			fs.rename(oldPath, newPath, function (error) {
+				console.info(newPath)
+			})
+		}
+		response.writeHead(200, {
+			"Content-Type": "text/html;charset=utf-8"
+		})
+		response.end("OK");
+	})
+});
+
+function getLocalIP() {
+    const os = require('os');
+    const osType = os.type(); //系统类型
+    const netInfo = os.networkInterfaces(); //网络信息
+    let ip = '';
+    if (osType === 'Windows_NT') { 
+        for (let dev in netInfo) {
+        	//win7的网络信息中显示为本地连接，win10显示为以太网
+            if (dev === '本地连接' || dev === '以太网') {
+                for (let j = 0; j < netInfo[dev].length; j++) {
+                    if (netInfo[dev][j].family === 'IPv4') {
+                        ip = netInfo[dev][j].address;
+                        break;
+                    }
+                }
+            }
+        }
+
+    } else if (osType === 'Linux') {
+        ip = netInfo.eth0[0].address;
+    }
+
+    return ip;
+}
+
+app.use(express.static(path.join(process.cwd(),"wwwroot"))); //wwwroot代表http服务器根目录
+
+var server = app.listen(3888, function () {
+	const host = server.address().address
+	const port = server.address().port
+	console.log("启动本地web服务(http://127.0.0.1:3888)成功！")
+	/*console.log("监听 http://%s:%s", "127.0.0.1", port)
+	console.log("监听 http://%s:%s", "localhost", port)
+	console.log("监听 http://%s:%s", getLocalIP(), port)
+	console.log("获取文件列表地址为 http://%s:%s/FileList", getLocalIP(), port)
+	console.log("下载地址为 http://%s:%s/Download/*文件名", getLocalIP(), port)
+	console.log("上传地址为 http://%s:%s/Upload", getLocalIP(), port)*/
+
+	
+});
---
+++ oaassist/server/wwwroot/resource/js/et.js
@@ -0,0 +1,86 @@
+var _et = {}
+
+function _EtStartUp(funcs) {
+    var info = {};
+
+    info.funcs = funcs;
+    WpsStartUp.StartUp(WpsStartUp.ClientType.et, // 组件类型
+        "etoa", // 插件名，与wps客户端加载的加载的插件名对应
+        "dispatcher", // 插件方法入口，与wps客户端加载的加载的插件代码对应，详细见插件代码
+        info, // 传递给插件的数据
+        function (result) { // 调用回调，status为0为成功，其他是错误
+            if (result.status)
+                alert(result.message)
+        })
+}
+
+function newDoc() {
+    _EtStartUp([{
+        "OpenDoc": {
+            showButton: "btnSaveFile;btnSaveAsLocal",
+        }
+    }])
+}
+
+_et['newDoc'] = {
+    action: newDoc,
+    code: _EtStartUp.toString() + "\n\n" + newDoc.toString(),
+    detail: "\n\
+  说明：\n\
+    点击按钮，打开表格组件后,新建一个空白表格文档\n\
+    \n\
+  方法使用：\n\
+    页面点击按钮，通过wps客户端协议来启动表格组件，调用oaassist插件，执行插件中的js函数OpenDoc,不带文档路径则默认新建一个空白表格文档\n\
+    funcs参数说明:\n\
+        OpenDoc方法对应于OA助手dispatcher支持的方法名\n\
+            showButton 要显示的按钮\n\
+"
+}
+
+function openDoc() {
+    var filePath = prompt("请输入打开文件路径（本地或是url）：")
+    var uploadPath = prompt("请输入文档上传路径:")
+
+    _EtStartUp([{
+        "OpenDoc": {
+            "docId": "123", // 文档ID
+            "uploadPath": uploadPath, // 保存文档上传路径
+            "fileName": filePath,
+            showButton: "btnSaveFile;btnSaveAsLocal",
+        }
+    }])
+}
+
+_et['openDoc'] = {
+    action: openDoc,
+    code: _EtStartUp.toString() + "\n\n" + openDoc.toString(),
+    detail: "\n\
+  说明：\n\
+    点击按钮，输入要打开的文档路径，输入文档上传路径，如果传的不是有效的服务端地址，将无法使用保存上传功能。\n\
+    打开表格组件后,将根据文档路径下载并打开对应的文档，保存将自动上传指定服务器地址\n\
+    \n\
+  方法使用：\n\
+    页面点击按钮，通过wps客户端协议来启动表格组件，调用oaassist插件，执行传输数据中的指令\n\
+    funcs参数信息说明:\n\
+        OpenDoc方法对应于OA助手dispatcher支持的方法名\n\
+            docId 文档ID，OA助手用以标记文档的信息，以区分其他文档\n\
+            uploadPath 保存文档上传路径\n\
+            fileName 打开的文档路径\n\
+            showButton 要显示的按钮\n\
+"
+}
+
+window.onload = function () {
+    var btns = document.getElementsByClassName("btn");
+
+    for (var i = 0; i < btns.length; i++) {
+        btns[i].onclick = function (event) {
+            document.getElementById("blockFunc").style.visibility = "visible";
+            var btn2 = document.getElementById("demoBtn");
+            btn2.innerText = this.innerText;
+            document.getElementById("codeDes").innerText = _et[this.id].detail.toString()
+            document.getElementById("code").innerText = _et[this.id].code.toString()
+            document.getElementById("demoBtn").onclick = _et[this.id].action;
+        }
+    }
+}
