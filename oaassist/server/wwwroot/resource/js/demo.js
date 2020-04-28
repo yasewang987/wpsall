7c6ad78d1898f34e7a2a29a39066f3a108598a99
rencaiying <1591504638@qq.com>
2020-04-28 09:07:34 +0800

    index.html页面demo.html页面做了做了http对象获取的兼容

--- oaassist/server/wwwroot/resource/js/demo.js
+++ oaassist/server/wwwroot/resource/js/demo.js
@@ -3,11 +3,11 @@ let isSetupOk = false
 function envTest(){
     //1.服务端检测
     
-    let xhr = new XMLHttpRequest()
+    let xhr = getHttpObj()
     xhr.onload=e=>{
         isServerOk = true
         //2.安装包检测
-        let xhr1 = new XMLHttpRequest()
+        let xhr1 = getHttpObj()
         xhr1.onload=e=>{
             if(xhr1.responseText.indexOf('异常')>-1||xhr1.responseText.indexOf('未安装')>-1||xhr1.responseText.indexOf('失败')>-1){
                 window.location.href="./demo.html"
@@ -74,4 +74,67 @@ window.onload = function () {
     var opts = app.getElementsByTagName("option"); //得到数组option
     opts[0].selected = true;
     envTest()
+}
+function IEVersion() {
+    var userAgent = navigator.userAgent; //取得浏览器的userAgent字符串  
+    var isIE = userAgent.indexOf("compatible") > -1 && userAgent.indexOf("MSIE") > -1; //判断是否IE<11浏览器  
+    var isEdge = userAgent.indexOf("Edge") > -1 && !isIE; //判断是否IE的Edge浏览器  
+    var isIE11 = userAgent.indexOf('Trident') > -1 && userAgent.indexOf("rv:11.0") > -1;
+    if (isIE) {
+        var reIE = new RegExp("MSIE (\\d+\\.\\d+);");
+        reIE.test(userAgent);
+        var fIEVersion = parseFloat(RegExp["$1"]);
+        if (fIEVersion == 7) {
+            return 7;
+        } else if (fIEVersion == 8) {
+            return 8;
+        } else if (fIEVersion == 9) {
+            return 9;
+        } else if (fIEVersion == 10) {
+            return 10;
+        } else {
+            return 6; //IE版本<=7
+        }
+    } else if (isEdge) {
+        return 20; //edge
+    } else if (isIE11) {
+        return 11; //IE11  
+    } else {
+        return 30; //不是ie浏览器
+    }
+}
+function getHttpObj() {
+    var httpobj = null;
+    if (IEVersion() < 10) {
+        try {
+            httpobj = new XDomainRequest();
+        } catch (e1) {
+            httpobj = new createXHR();
+        }
+    } else {
+        httpobj = new createXHR();
+    }
+    return httpobj;
+}
+//兼容IE低版本的创建xmlhttprequest对象的方法
+function createXHR() {
+    if (typeof XMLHttpRequest != 'undefined') { //兼容高版本浏览器
+        return new XMLHttpRequest();
+    } else if (typeof ActiveXObject != 'undefined') { //IE6 采用 ActiveXObject， 兼容IE6
+        var versions = [ //由于MSXML库有3个版本，因此都要考虑
+            'MSXML2.XMLHttp.6.0',
+            'MSXML2.XMLHttp.3.0',
+            'MSXML2.XMLHttp'
+        ];
+
+        for (var i = 0; i < versions.length; i++) {
+            try {
+                return new ActiveXObject(versions[i]);
+            } catch (e) {
+                //跳过
+            }
+        }
+    } else {
+        throw new Error('您的浏览器不支持XHR对象');
+    }
 }
